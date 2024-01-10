78a8f53ae38e6e8046ce01725f5c0327b5166585
Muradmustafayev-03 <muradmustafayev03@gmail.com>
2024-01-10 13:11:38 +0400

    added audio_utils.py

---
+++ src/audio_utils.py
@@ -0,0 +1,25 @@
+import IPython.display as ipd
+
+
+def play_audio(array, sr=None):
+    """Play Audio with IPython.display.Audio
+
+    :param array: Audio array
+    :type array: numpy.ndarray
+    :param sr: Sampling rate
+    :type sr: int
+    """
+    ipd.display(ipd.Audio(array, rate=sr))
+
+
+def listen_to_segments(words, sr=None):
+    """Listen to audio segments
+
+    :param words: List of audio segments
+    :type words: list of numpy.ndarray
+    :param sr: Sampling rate
+    :type sr: int
+    """
+    for i, word in enumerate(words):
+        print(f'Listening to Word {i+1}')
+        play_audio(word, sr=sr)
